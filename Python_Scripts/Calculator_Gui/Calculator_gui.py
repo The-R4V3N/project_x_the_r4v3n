e5990572a0c98006e7142842aeed98b8810ffdce
Oliver Joisten <o.joisten@live.se>
2023-01-15 17:28:33 +0100

    Clean up

--- Python_Scripts/Calculator_Gui/Calculator_gui.py
+++ Python_Scripts/Calculator_Gui/Calculator_gui.py
@@ -136,12 +136,12 @@ filemenu.add_separator()
 filemenu.add_command(label="Exit", command=iExit)
 
 # ManuBar 2 :
-editmenu = Menu(menubar, tearoff=0)
-menubar.add_cascade(label='Edit', menu=editmenu)
-editmenu.add_command(label="Cut")
-editmenu.add_command(label="Copy")
-editmenu.add_separator()
-editmenu.add_command(label="Paste")
+# editmenu = Menu(menubar, tearoff=0)
+# menubar.add_cascade(label='Edit', menu=editmenu)
+# editmenu.add_command(label="Cut")
+# editmenu.add_command(label="Copy")
+# editmenu.add_separator()
+# editmenu.add_command(label="Paste")
 
 root.config(menu=menubar)
 
