0ee2def242ec321ffde704c50c1183f1069d2f9d
The-R4V3N <o.joisten@live.se>
2023-01-16 16:22:52 +0100

    Added main.cpp, main.h and Makefile

---
+++ Main_Components/main.cpp
@@ -0,0 +1,19 @@
+
+/**
+ * @file main.cpp
+ * @author Oliver Joisten (https://oliver-joisten.se/)
+ * @brief  The main Components of The Assignment
+ * @version 0.1
+ * @date 2023-01-16
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#include "main.h"
+#include <iostream>
+
+int main(void)
+{
+    return 0;
+}
---
+++ Main_Components/main.h
@@ -0,0 +1,19 @@
+
+/**
+ * @file main.h
+ * @author Oliver Joisten (https://oliver-joisten.se/)
+ * @brief  The Header file for the main Component
+ * @version 0.1
+ * @date 2023-01-16
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+class Main_component
+{
+};
+#endif // MAIN_H
