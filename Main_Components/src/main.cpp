25bafaccc259963152477599d2dc95e2fcc7b61e
The-R4V3N <o.joisten@live.se>
2023-02-14 13:32:41 +0100

    Added input txt file
    -Made a folder input with the corresponding txt file which is needed for run the project
    - Changed the comment from output to input for the command to rund the project

--- Main_Components/src/main.cpp
+++ Main_Components/src/main.cpp
@@ -387,5 +387,5 @@ int main(int argc, char *argv[])
 }
 /*
 Command to run after compiling
-./Main_Components/main_component ../Main_Components/output/signals.txt ./output.json
+./Main_Components/main_component ../Main_Components/intput/signals.txt ./output.json
 */
